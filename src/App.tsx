402e76a3070b6e0d1cb17c5f8ef6419ef91568d1
Trung <tly@ncswash.com>
2024-09-20 19:29:13 -0500

    website overhaul

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import './App.css'; 
+
+
+
+const App: React.FC = () => {
+  return (
+    
+      <div>
+        <h1 className="nametxt">Trung Ly </h1>
+        <div className="rectangle-1"></div>
+    </div>
+  
+   
+  );
+};
+
+
+export default App;
