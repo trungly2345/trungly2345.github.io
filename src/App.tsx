8ecc99fd499d9e456c29f4b5398a838baeaa7b7c
Trung <tly@ncswash.com>
2024-09-22 00:43:09 -0500

    implemented github icon and made it clickable, also added a header statement along with a computer svg icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
-import React from "react"
-import './App.css'; 
-import il from './assets/li.svg';
-
-
-
-
+import React from "react";
+import "./App.css";
+import il from "./assets/li.svg";
+import github from "./assets/github.png";
+import mycomp from "./assets/My Computer.svg";
 
 const App: React.FC = () => {
-
- 
   return (
-    
-      <div>
-        <h1 className="nametxt">Trung Ly </h1>
-        <div className="rectangle-1"></div>
-        <a href="https://www.linkedin.com/in/trung-ly-30151b184/" target="blank" rel="noopener noreferrer">
-        <img 
-        src= {il} 
-        alt="LinkedIn"
-        className="li.svg" 
-        style={{position:'absolute',width: '60px',height:'36px',top:'60px',left:'115px'}}
-        
+    <div>
+      <h1 className="nametxt">Trung Ly </h1>
+      <div className="rectangle-1"></div>
+      <a
+        href="https://www.linkedin.com/in/trung-ly-30151b184/"
+        target="blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={il}
+          alt="LinkedIn"
+          className="li.svg"
+          style={{
+            position: "fixed",
+            width: "60px",
+            height: "36px",
+            top: "60px",
+            left: "115px",
+          }}
         />
-        </a>
+      </a>
 
-        
+      <a
+        href="https://github.com/trungly2345/portfolioWebApp"
+        target="blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={github}
+          alt="GitHub"
+          className="github.png"
+          style={{
+            position: "fixed",
+            width: "45px",
+            height: "36px",
+            top: "60px",
+            left: "170px",
+          }}
+        />
+      </a>
+
+      <h1 className="opener">Welcome to my portfolio website!</h1>
+      <img src={mycomp} style={{position:"absolute",top:"85px", left:"990px", width:"65px",height:"auto"}}/>
     </div>
   );
 };
-
 
 export default App;
